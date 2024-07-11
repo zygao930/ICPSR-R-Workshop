9444b08d15e16f133c908985566683fd8391e86c
Claire_Gao0930 <13524598407@126.com>
2024-07-11 13:25:36 +0800

    update init

---
+++ course-code.R
@@ -0,0 +1,15 @@
+2+7
+17/3
+2^3
+
+sum<-2+5
+sum
+
+sum^2
+
+library(tidyr)
+
+setwd("F:/ICPSRworkshop/Introduction to the R Statistical Computing Environment/wd")
+getwd()
+
+mydata<-read.csv("oda_crs_oecd2012_2.csv")
